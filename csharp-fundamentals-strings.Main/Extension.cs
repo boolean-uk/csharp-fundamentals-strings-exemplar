78a7d071fd1d51688250642c42540e6c2b490bd2
uerbzr <nigel.sibbert@gmail.com>
2023-03-28 16:24:52 +0100

    Completed exemplar

--- csharp-fundamentals-strings.Main/Extension.cs
+++ csharp-fundamentals-strings.Main/Extension.cs
@@ -43,7 +43,7 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+            sb.Append("Hello, world!");
 
             // ...AND THIS LINE
 
@@ -58,7 +58,7 @@ namespace csharp_fundamentals_strings.Main
             // 2. After adding the message, use an appropriate StringBuilder method to reverse it
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+            sb.Append("Hello, world!".ReverseMe());
 
             // ...AND THIS LINE
 
@@ -73,7 +73,9 @@ namespace csharp_fundamentals_strings.Main
             // 2. After adding the message, remove the comma.
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
+            sb.Append("Hello, world!");
 
+            sb.Remove(sb.ToString().IndexOf(','), 1);
 
             // ...AND THIS LINE
 
@@ -87,7 +89,8 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // 2. After adding the message, replace the word "world" with the word "C#"
             // WRITE YOUR CODE BETWEEN THIS LINE...
-
+            sb.Append("Hello, world!");
+            sb.Replace("world", "C#");
 
 
             // ...AND THIS LINE
---
+++ csharp-fundamentals-strings.Main/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals_strings.Main
+{
+    public static class ExtensionMethods
+    {
+        public static string ReverseMe(this string str)
+        {
+            return new string(str.Reverse().ToArray());
+        }
+    }
+}
